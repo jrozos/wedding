d2a66aa8ebaad8ae3e7723985f51a630ce3c37ec
jrozos <sonarozos@gmail.com>
2022-11-14 18:12:29 -0600

    Test git ftp

--- resources/views/header/header.blade.php
+++ resources/views/header/header.blade.php
@@ -6,7 +6,7 @@
         <div class="container">
             <div class="content">
                 <div class="title-area">
-                    <h1 class="title-modern">Gracias por asistir</h1>
+                    <h1 class="title-modern">Gracias por asistir.</h1>
                     <h3 class="text-primary">Nos casamos el 29 de Octubre 2022</h2>
                     <div class="separator line-separator">♦</div>
                 </div>
